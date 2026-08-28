448d20860012f47d5cbeffa77fc969034e5a6887
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-28 03:50:36 -0700

    feat(unreal): add runtime module

---
+++ clients/unreal/Source/CivilizationClient/CivilizationClient.cpp
@@ -0,0 +1,4 @@
+// clients/unreal/Source/CivilizationClient/CivilizationClient.cpp
+#include "CivilizationClient.h"
+#include "Modules/ModuleManager.h"
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, CivilizationClient, "CivilizationClient");
