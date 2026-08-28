e4cd6aefa4cef9c0bf8c569dad4d25b2ef46b2d1
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-27 21:35:20 -0700

    feat: expose v0.2 map primitives

--- src/civilization_clone/__init__.py
+++ src/civilization_clone/__init__.py
@@ -1,7 +1,9 @@
 """CivilizationClone deterministic 4X engine core."""
 
+from civilization_clone.domain.map import HexCoord, ResourceType, TerrainType, Tile, WorldMap
 from civilization_clone.domain.state import CoreGameState, GamePhase, GameStatus
 from civilization_clone.engine.event_log import EventLog, EventLogError
+from civilization_clone.engine.mapgen import MapGenerationConfig, MapGenerationResult, generate_world
 from civilization_clone.engine.rng import DeterministicRng, RngFactory
 from civilization_clone.engine.state_hash import canonical_json, state_hash
 from civilization_clone.rules.loader import RulesetLoader
@@ -14,11 +16,19 @@ __all__ = [
     "EventLogError",
     "GamePhase",
     "GameStatus",
+    "HexCoord",
+    "MapGenerationConfig",
+    "MapGenerationResult",
+    "ResourceType",
     "RngFactory",
     "RulesetLoader",
     "RulesetManifest",
+    "TerrainType",
+    "Tile",
+    "WorldMap",
     "canonical_json",
+    "generate_world",
     "state_hash",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
