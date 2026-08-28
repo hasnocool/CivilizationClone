1b354c5794e5dd3c0a3ec2bb2131968ff411c64a
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-27 23:12:15 -0700

    feat: export civilization content schemas

--- src/civilization_clone/rules/__init__.py
+++ src/civilization_clone/rules/__init__.py
@@ -1,1 +1,16 @@
 """Versioned ruleset schemas and loading utilities."""
+
+from civilization_clone.rules.civilizations import (
+    CivilizationDefinition,
+    load_civilizations,
+)
+from civilization_clone.rules.loader import RulesetLoadError, RulesetLoader
+from civilization_clone.rules.schemas import RulesetManifest
+
+__all__ = [
+    "CivilizationDefinition",
+    "RulesetLoadError",
+    "RulesetLoader",
+    "RulesetManifest",
+    "load_civilizations",
+]
