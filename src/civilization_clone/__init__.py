21997c6894b6bd3006b3650149cba33c76f7c0c4
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-27 21:17:42 -0700

    feat: export deterministic event journal

--- src/civilization_clone/__init__.py
+++ src/civilization_clone/__init__.py
@@ -1,6 +1,7 @@
 """CivilizationClone deterministic 4X engine core."""
 
 from civilization_clone.domain.state import CoreGameState, GamePhase, GameStatus
+from civilization_clone.engine.event_log import EventLog, EventLogError
 from civilization_clone.engine.rng import DeterministicRng, RngFactory
 from civilization_clone.engine.state_hash import canonical_json, state_hash
 from civilization_clone.rules.loader import RulesetLoader
@@ -9,6 +10,8 @@ from civilization_clone.rules.schemas import RulesetManifest
 __all__ = [
     "CoreGameState",
     "DeterministicRng",
+    "EventLog",
+    "EventLogError",
     "GamePhase",
     "GameStatus",
     "RngFactory",
